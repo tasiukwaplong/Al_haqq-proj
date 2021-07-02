5c3dc8c01f379efd7c1886109dc9ea8764e7cfb7
Cephas Osayi <cephasosayi@gmail.com>
2021-07-02 11:52:59 +0100

    first commit

---
+++ js/cart.js
@@ -0,0 +1,14 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+for (i = 0; i < updateBtns.length; i++) {
+	updateBtns[i].addEventListener('click', function(){
+		var productId = this.dataset.product
+		var action = this.dataset.action
+        console.log('productId:', productId, 'Action:', action)
+        console.log('USER', user)
+        if(USER==='AnanymousUser'){
+            console.log('Not logged in')
+        }else{
+            console.log('User is logged in, sending data...')
+        }
+        })
+}
